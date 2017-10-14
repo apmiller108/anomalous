9c4406a9a7dcf7eebf6640dd55c193f6c55d15f3
Alex Miller <apmiller108@yahoo.com>
2017-10-14 18:20:27 -0400

    Extract blocks to methods

--- lib/anomalous/epsilon.rb
+++ lib/anomalous/epsilon.rb
@@ -1,29 +1,43 @@
 module Anomalous
   class Epsilon
-    def initialize(cross_val_set, pval)
-      @y_labels = extract_lables(cross_val_set)
-      @pval     = pval
+    def initialize(examples, probabilities)
+      @y_labels = extract_lables(examples)
+      @prob     = probabilities
     end
 
     def select_threshold
-      scores = []
-      flat_array.min.step(to: flat_array.max, by: step_size) do |epsilon|
-        scores << [
-          epsilon,
-          Anomalous::Score::Scoring.new(@y_labels, @pval, epsilon).f1
-        ]
-      end
-      scores
+      flat_array
+        .min
+        .step(to: flat_array.max, by: step_size)
+        .each_with_object([], &method(:collect_scores))
+        .delete_if(&method(:f1_is_nan?))
+        .sort(&method(:by_f1_score_desc))
+        .last
     end
 
     private
+
+    def by_f1_score_desc(x, y)
+      x[1] <=> y[1]
+    end
+
+    def f1_is_nan?(array)
+      array[1].nan?
+    end
+
+    def collect_scores(epsilon, array)
+      array << [
+        epsilon,
+        Anomalous::Score::Scoring.new(@y_labels, @prob, epsilon).f1
+      ]
+    end
 
     def step_size
       (flat_array.max - flat_array.min) / 1000
     end
 
     def flat_array
-      @flat_array ||= @pval.to_flat_array
+      @flat_array ||= @prob.to_flat_array
     end
 
     def extract_lables(cross_val_set)
